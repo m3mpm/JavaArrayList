410f3903be09bbccb05e3605cea0ae0261a7bbea
Ruslan <m3mpm@users.noreply.github.com>
2025-03-30 15:40:40 +0700

    add tests

---
+++ src/test/java/org/m3mpm/CarListInterfaceTest.java
@@ -0,0 +1,69 @@
+package org.m3mpm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarListInterfaceTest {
+
+    private CarListInterface carListInterface;
+
+    @BeforeEach
+    void setUp() {
+        for (int i = 0; i < 10; i++) {
+            carListInterface.add(new Car("brand" + i, i));
+        }
+    }
+
+    @Test
+    void get(){
+        Car car = carListInterface.get(0);
+        assertEquals("brand0", car.getBrand());
+    }
+
+    @Test
+    void add(){
+        assertEquals(10, carListInterface.size());
+    }
+
+    @Test
+    void removeByElementTrue(){
+        Car car = new Car("LocalBrand", 100);
+        carListInterface.add(car);
+        assertEquals(11, carListInterface.size());
+        assertTrue(carListInterface.remove(car));
+        assertEquals(10, carListInterface.size());
+    }
+
+    @Test
+    void removeByElementFalse(){
+        Car car = new Car("LocalBrand", 0);
+        assertFalse(carListInterface.remove(car));
+        assertEquals(10, carListInterface.size());
+    }
+
+    @Test
+    void removeByIndexTrue(){
+        assertTrue(carListInterface.remove(2));
+        assertEquals(9, carListInterface.size());
+    }
+
+    @Test
+    void removeByIndexFalse(){
+        assertEquals(10, carListInterface.size());
+        assertFalse(carListInterface.remove(20));
+    }
+
+    @Test
+    void removeAll(){
+        carListInterface.clear();
+        assertEquals(0, carListInterface.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    void whenIndexOutOfBoundsThenThrowException(){
+        carListInterface.get(20);
+    }
+
+}
